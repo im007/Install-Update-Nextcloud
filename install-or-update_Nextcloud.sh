da3f257931e2f8c6169f72df5c6dd546c92c5c6e
im007 <33734981+im007@users.noreply.github.com>
2023-04-09 15:26:37 -0700

    Update install-or-update_Nextcloud.sh

--- install-or-update_Nextcloud.sh
+++ install-or-update_Nextcloud.sh
@@ -3,14 +3,14 @@
 # Fetch latest Nextcloud release info from GitHub's API
 LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/nextcloud/desktop/releases/latest)
 
-# Extract the version number and remove the "v" prefix
+# Extract the version number + remove "v" prefix
 LATEST_VERSION=$(echo "$LATEST_RELEASE_JSON" | jq -r '.tag_name' | sed 's/^v//')
 
-# Define location for the Nextcloud AppImage
+# Define location for Nextcloud AppImage
 NEXTCLOUD_LOC="/home/$USER/.local/share/applications"
 APPIMAGE_PATH="$NEXTCLOUD_LOC/Nextcloud.AppImage"
 
-# Check if the AppImage exists, extract the version number, and compare it with the latest version
+# Check if AppImage exists; extract version number & compare with the latest version
 INSTALL_UPDATE=false
 if [ -f "$APPIMAGE_PATH" ]; then
     INSTALLED_VERSION=$("$APPIMAGE_PATH" --version | grep version | cut -d' ' -f3)
@@ -24,7 +24,7 @@ else
 fi
 
 if [ "$INSTALL_UPDATE" = true ]; then
-    # Build the URL for the AppImage file, download it & save as Nextcloud.AppImage
+    # Build the URL for the AppImage file, download & save as Nextcloud.AppImage
     APPIMAGE_URL="https://github.com/nextcloud/desktop/releases/download/v${LATEST_VERSION}/Nextcloud-${LATEST_VERSION}-x86_64.AppImage"
     wget -O Nextcloud.AppImage "$APPIMAGE_URL"
 
